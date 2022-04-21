58c4c0306553efb65b29e7d9a0dece30da354f45
deamondev <deamondev@gmail.com>
2022-04-21 17:19:57 +0200

    solved problem #25

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -32,19 +32,28 @@ randomlySelect n xs = do
   print randomVals
 
 -- Problem 24 --
-diffSelectHelper :: StdGen -> Int -> [Int] -> [Int] -> IO ()
+diffSelectHelper :: StdGen -> Int -> [Int] -> [Int] -> IO [Int]
 diffSelectHelper gen k currentList acc = do
   if k == 0 
-    then print acc
+    then return acc
   else do 
     let (newIndex, newGen) = randomR (0, length currentList - 1) gen :: (Int, StdGen)
     let newList = removeAt currentList (newIndex + 1)
     diffSelectHelper newGen (k - 1) newList ((currentList !! newIndex):acc)
 
-diffSelect :: Int -> Int -> IO ()
+diffSelect :: Int -> Int -> IO [Int]
 diffSelect n m = do 
   gen       <- getStdGen 
   diffSelectHelper gen n [1..m] []
+
+-- Problem 25 -- 
+randomPermutation :: String -> IO () 
+randomPermutation s = do 
+  let len = length s
+  p      <- diffSelect len len 
+  let p'   = map (\x -> x - 1) p 
+  print $ [s !! i | i <- p']
+
 
 
 
