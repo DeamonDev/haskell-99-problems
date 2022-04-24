a761631e0e8574a803b4d817fa50086d7e3a5b0c
deamondev <deamondev@gmail.com>
2022-04-24 12:02:14 +0200

    solved problem #56

--- src/Chapter6.hs
+++ src/Chapter6.hs
@@ -12,12 +12,25 @@ leaf x = Branch x Empty Empty
 -- Problem 55 --
 cbalTrees :: Int -> [Tree Char]
 cbalTrees 1 = [Branch 'x' Empty Empty]
-cbalTrees n 
+cbalTrees n
   | even n    = [Branch 'x' l r | l <- cbalTrees $ n `div` 2
                                 , r <- cbalTrees $ n `div` 2]
   | otherwise = [Branch 'x' l r | l <- cbalTrees $ (n - 1) `div` 2
                                 , r <- cbalTrees $ (n + 1) `div` 2]  ++
                 [Branch 'x' r l | l <- cbalTrees $ (n - 1) `div` 2
-                                , r <- cbalTrees $ (n + 1) `div` 2]  
+                                , r <- cbalTrees $ (n + 1) `div` 2]
 
+
+-- Problem 56 -- 
+mirror :: Tree a -> Tree a -> Bool
+mirror Empty              Empty            = True
+mirror (Branch _ l r)     (Branch _ l' r') = (mirror l l') && (mirror r r')
+mirror (Branch {} )       Empty            = False
+mirror Empty              (Branch {})      = False
+
+symmetric :: Tree a -> Bool
+symmetric Empty = True
+symmetric (Branch _ l r) = mirror l r
+
+-- Problem 57 -- 
 
